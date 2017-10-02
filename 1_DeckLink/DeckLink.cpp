72c41ee4fa7b0c069e5616cadba2fa76bb8b66c7
Dan Craft <simputest@gmail.com>
2017-10-02 23:57:44 +0800

    DeckLink example with midl template on Windows

--- 1_DeckLink/DeckLink.cpp
+++ 1_DeckLink/DeckLink.cpp
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <objbase.h>
+
+#include "DeckLinkAPI.h"
 
 int main(int argc, char** argv)
 {
     printf("%s Starts!\n", argv[0]);
- 
+    CoInitialize(NULL);
+
+    IDeckLinkIterator* deckLinkIterator = NULL;
+    if (S_OK == CoCreateInstance(CLSID_CDeckLinkIterator, NULL, CLSCTX_ALL, IID_IDeckLinkIterator, reinterpret_cast<void**>(&deckLinkIterator)))
+    {
+        IDeckLink* deckLinkInstance = NULL;
+        if (S_OK == deckLinkIterator->Next(&deckLinkInstance))
+        {
+            deckLinkInstance->Release();
+            deckLinkInstance = NULL;
+        }
+        deckLinkIterator->Release();
+        deckLinkIterator = NULL;
+    }
+
+    CoUninitialize();
     printf("%s Completes!\n", argv[0]);
     return 1;
 }
